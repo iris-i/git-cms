73103deabd2f676b0ea2a4aaf8f3c3b70b9a0d3b
Iris Ibekwe <iris.ibekwe@civicactions.com>
2024-11-11 04:28:37 -0500

    Add the admin navbar.

---
+++ src/app/components/nav/NavDrop.tsx
@@ -0,0 +1,22 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+export default function NavDrop({ children }: { children: React.ReactNode }) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>{children}</DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Logout</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
--- src/app/components/nav/Navbar.tsx
+++ src/app/components/nav/Navbar.tsx
@@ -1,6 +1,18 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Link from "next/link";
+import NavDrop from "./NavDrop";
+
 function Navbar() {
   return (
-    <nav className="bg-primary flex justify-between">Navbar</nav>
+    <nav className="bg-primary flex justify-between text-primary-foreground py-2 px-5">
+      <Link href="/dashboard">Admin Dashboard</Link>
+      <NavDrop>
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" alt="avatar" />
+          <AvatarFallback>II</AvatarFallback>
+        </Avatar>
+      </NavDrop>
+    </nav>
   );
 }
 
